9f6453e5773ae4c308285b3fc3a00b7ac0a4b7d3
julia <juliiagordin@gmail.com>
2023-08-31 19:33:52 +0300

    add class LoginTestStart

--- src/test/java/start/LoginTestStart.java
+++ src/test/java/start/LoginTestStart.java
@@ -1,2 +1,33 @@
-package start;public class LoginTestStart {
+package start;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginTestStart {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void preconditions() {
+        driver = WebDriverManager.firefoxdriver().create();
+        driver.navigate().to("https://trello.com/");
+        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void loginPositive() {
+
+    }
+
+    @AfterClass
+    public void postConditions() {
+      //  driver.quit();
+    }
+
+
 }
